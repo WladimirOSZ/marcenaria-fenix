6fdf035ad3532ebddd59d5302bcdadfad6e00e53
Wladimir Souza <wladimir5002@gmail.com>
2024-11-09 17:21:11 -0300

    refactor: remove the need to have separate html files to load modal images

--- script.js
+++ script.js
@@ -1,67 +1,104 @@
 $('[data-toggle="tooltip"]').tooltip();
 
-// Carousel
-var carouselLength = $('.carousel-item').length - 1;
-let counterLoad = new Array(5);
-let counterMax = new Array(5);
-// tooltip
+let carouselLength = $('.carousel-item').length - 1;
 const categoriesNames = ['geral', 'salas', 'cozinhas', 'quartos', 'banheiros', 'outros'];
-for (var i = 0; i < 6; i++) {
-    counterLoad[categoriesNames[i]] = 0;
+
+const modalImages = {
+    'geral': {
+        0: ['1', '6', '2', '3', '20', '4', '21', '5', '8', '9'],
+        1: ['7', '10', '11', '12', '13', '18', '15', '19', '17', '14'],
+        2: ['22', '23', '24', '25', '26', '27', '28', '29']
+    },
+    'salas': {
+        0: ['4', '8', '2', '3', '5', '11', '12', '14'],
+        1: ['6', '9', '10', '1', '7', '13']
+    },
+    'cozinhas': {
+        0: ['16', '17', '3', '4', '5', '6', '7', '8', '18', '20'],
+        1: ['9', '10', '15', '11', '12', '13', '19', '21', '22'],
+        2: ['29', '26', '23', '24', '25', '27', '28']
+    },
+    'quartos': {
+        0: ['31', '32', '18', '19', '20', '8', '9', '10', '15', '16'],
+        1: ['11', '12', '13', '14', '2', '3', '4', '5', '6', '7', '33'],
+        2: ['17', '21', '22', '23', '24', '25', '26', '27', '28']
+    },
+    'banheiros': {
+        0: ['6', '9', '8', '7', '4', '3', '2', '1', '5'],
+    },
+    'outros': {
+        0: ['1', '10', '3', '4', '9', '2', '11', '5', '24'],
+        1: ['12', '18', '14', '13', '15', '16', '17', '19', '20', '21', '22', '23', '25']
+    }
 }
-counterMax[0] = 1;
-counterMax[1] = 1;
-counterMax[2] = 2;
-counterMax[3] = 2;
-counterMax[4] = 0;
-counterMax[5] = 1;
 
 const categories = {
     'geral': {
         counterMax: 1,
-        categoryNumber: 0
+        categoryNumber: 0,
+        counterLoad: 0
     },
     'salas': {
         counterMax: 1,
-        categoryNumber: 1
+        categoryNumber: 1,
+        counterLoad: 0
     },
     'cozinhas': {
         counterMax: 2,
-        categoryNumber: 2
+        categoryNumber: 2,
+        counterLoad: 0
     },
     'quartos': {
         counterMax: 2,
-        categoryNumber: 3
+        categoryNumber: 3,
+        counterLoad: 0
     },
     'banheiros': {
         counterMax: 0,
-        categoryNumber: 4
+        categoryNumber: 4,
+        counterLoad: 0
     },
     'outros': {
         counterMax: 1,
-        categoryNumber: 5
+        categoryNumber: 5,
+        counterLoad: 0
     }
+}
+
+const loadModalImages = (categoryLabel) => {
+    const category = categories[categoryLabel];
+    modalImages[categoryLabel][category.counterLoad].forEach((image, idx) => {
+        const isActive = category.counterLoad === 0 && idx === 0 ? 'active' : '';
+        $('#carousel').append(`
+            <div class="carousel-item ${isActive}">
+                <img src="fotos/moveis/${categoryLabel}/${categoryLabel} (${image}).jpg" class="imgModal mx-auto">
+            </div>
+        `)
+    })
 }
 
 function upperCaseFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function selectCategory(category) {
+function selectCategory(categoryLabel) {
     $("#dropdownNav a").removeClass("active");
     $("#dropdownButton a").removeClass("active");
     $("#carousel").empty();
     $("#images").empty();
 
-    $("#carousel").load(`components/modalImages/${category}.html`)
+    const category = categories[categoryLabel];
+    category.counterLoad = 0;
+    loadModalImages(categoryLabel)
     // check if this function after this thing is really needed
-    $("#images").load(`components/sectionImages/${category}.html`)
-    if (category === 'banheiros') $('#loadMoreButton').addClass('d-none')
+    $("#images").load(`components/sectionImages/${categoryLabel}.html`)
+    if (categoryLabel !== 'banheiros') $('#loadMoreButton').removeClass('d-none')
+    if (categoryLabel === 'banheiros') $('#loadMoreButton').addClass('d-none')
 }
 
 function updateVariables(elementSelected) {
     const category = $(elementSelected).text().toLowerCase();
-    counterLoad[category] = 0;
+    categories[category].counterLoad = 0;
     $('.carousel-control-next').removeClass('d-none');
     carouselLength = $('.carousel-item').length - 1;
     elementSelected.classList.add('active');
@@ -69,23 +106,22 @@ function updateVariables(elementSelected) {
 }
 
 function addMore() {
-    const category = getCategory();
-    const pageToLoad = category + counterLoad[category] + ".html";
+    const categoryLabel = getCategory();
+    const category = categories[categoryLabel];
+    const pageToLoad = categoryLabel + category.counterLoad + ".html";
 
     if (pageToLoad) {
-        $.get("components/modalImages/" + pageToLoad, function (content) {
-            $('#carousel-inner').append(content);
-            $('.carousel-control-next').removeClass('d-none');
-            carouselLength = $('.carousel-item').length - 1;
-            $('.carousel-control-next-icon').removeClass('addMoreIcon');
-            $("#carouselImages").carousel("next");
-        });
+        category.counterLoad++;
+        loadModalImages(categoryLabel)
+        $('.carousel-control-next').removeClass('d-none');
+        carouselLength = $('.carousel-item').length - 1;
+        $('.carousel-control-next-icon').removeClass('addMoreIcon');
+        $("#carouselImages").carousel("next");
         $.get("components/sectionImages/" + pageToLoad, function (content) {
             $('#images').append(content);
         });
-        counterLoad[category]++;
 
-        if (counterLoad[category] >= categories[category].counterMax) {
+        if (category.counterLoad >= category.counterMax) {
             $('#loadMoreButton').addClass('d-none');
         }
     }
@@ -96,6 +132,9 @@ function getCategory() {
 }
 
 $(document).ready(function () {
+    $("#images").load("components/sectionImages/geral.html");
+    loadModalImages('geral')
+
     $("#carouselImages").carousel({ wrap: false }).on('slide.bs.carousel', function (e) {
         carouselLength = $('.carousel-item').length - 1;
         if (e.to == 0) {
@@ -104,8 +143,9 @@ $(document).ready(function () {
             $('.carousel-control-next-icon').removeClass('addMoreIcon');
         } else if (e.to == carouselLength) {
             $('.carousel-control-prev').removeClass('d-none');
-            let category = getCategory();
-            if (counterLoad[category] < counterMax[category]) {
+            const categoryLabel = getCategory();
+            const category = categories[categoryLabel];
+            if (category.counterLoad < category.counterMax) {
                 $('.carousel-control-next-icon').addClass('addMoreIcon');
             } else {
                 $('.carousel-control-next').addClass('d-none');
@@ -123,7 +163,6 @@ $(document).ready(function () {
             addMore();
         }
     });
-
 
     // select category
     $("#dropdownNav a").click(function () {
